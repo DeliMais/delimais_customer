45c797613b6aac0e7b7602c25d03bddb15f78531
João Sereia <joao.lo.sereia@gmail.com>
2024-11-26 15:27:22 -0300

    feat(widgets/page): add auto control of status bar brightness according to theme

--- lib/core/widgets/page_widget.dart
+++ lib/core/widgets/page_widget.dart
@@ -1,5 +1,6 @@
 import 'package:delimais_customer/core/mixins/theme_mixin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PageWidget extends StatelessWidget with ThemeMixin {
   const PageWidget({
@@ -17,12 +18,24 @@ class PageWidget extends StatelessWidget with ThemeMixin {
 
   @override
   Widget build(BuildContext context) {
+    final theme = getThemeData(context);
     final (_, metrics) = getTheme(context);
+
+    var barBrightness = Brightness.light;
+    var barIconBrightness = Brightness.dark;
+    switch (theme.brightness) {
+      case Brightness.light:
+        barBrightness = theme.brightness;
+        barIconBrightness = Brightness.dark;
+      case Brightness.dark:
+        barBrightness = Brightness.dark;
+        barIconBrightness = theme.brightness;
+    }
 
     return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: true,
-      body: Center(child: body),
+      resizeToAvoidBottomInset: false,
       bottomNavigationBar: navBar,
       appBar: appBar != null
           ? PreferredSize(
@@ -30,6 +43,17 @@ class PageWidget extends StatelessWidget with ThemeMixin {
               child: appBar!,
             )
           : null,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarBrightness: barBrightness,
+          statusBarIconBrightness: barIconBrightness,
+          systemNavigationBarColor: Colors.transparent,
+          systemNavigationBarIconBrightness: barIconBrightness,
+          systemNavigationBarDividerColor: Colors.transparent,
+        ),
+        child: Center(child: body),
+      ),
     );
   }
 }
