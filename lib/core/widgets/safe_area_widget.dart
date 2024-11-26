f7017f2830fb70604054018540379021eb87ad54
João Sereia <joao.lo.sereia@gmail.com>
2024-11-26 15:26:10 -0300

    feat(widgets/safe_area): add minimum padding

--- lib/core/widgets/safe_area_widget.dart
+++ lib/core/widgets/safe_area_widget.dart
@@ -1,12 +1,14 @@
+import 'package:delimais_customer/core/mixins/theme_mixin.dart';
 import 'package:flutter/material.dart';
 
-class SafeAreaWidget extends StatelessWidget {
+class SafeAreaWidget extends StatelessWidget with ThemeMixin {
   const SafeAreaWidget({
     required this.child,
     this.top = false,
     this.bottom = false,
     this.left = false,
     this.right = false,
+    this.showMinimum = true,
     super.key,
   });
 
@@ -15,14 +17,29 @@ class SafeAreaWidget extends StatelessWidget {
   final bool bottom;
   final bool left;
   final bool right;
+  final bool showMinimum;
 
   @override
   Widget build(BuildContext context) {
+    final (_, metrics) = getTheme(context);
+    final safeArea = MediaQuery.of(context).padding;
+
+    var padding = EdgeInsets.zero;
+    if (showMinimum) {
+      padding = padding.copyWith(
+        top: top && safeArea.top == 0 ? metrics.medium : null,
+        bottom: bottom && safeArea.bottom == 0 ? metrics.medium : null,
+        left: left && safeArea.left == 0 ? metrics.medium : null,
+        right: right && safeArea.right == 0 ? metrics.medium : null,
+      );
+    }
+
     return SafeArea(
       top: top,
       bottom: bottom,
       left: left,
       right: right,
+      minimum: padding,
       child: child,
     );
   }
