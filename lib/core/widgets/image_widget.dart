f1203d4f8148f34f31870eed696ddf13eb444f67
João Sereia <joao.lo.sereia@gmail.com>
2024-11-27 14:40:23 -0300

    feat(widgets/image): add border radius and fallback widget

--- lib/core/widgets/image_widget.dart
+++ lib/core/widgets/image_widget.dart
@@ -1,25 +1,56 @@
+import 'package:delimais_customer/core/mixins/theme_mixin.dart';
+import 'package:delimais_customer/core/widgets/container_widget.dart';
+import 'package:delimais_customer/core/widgets/icon_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:solar_icon_pack/solar_icon_pack.dart';
 
-class ImageWidget extends StatelessWidget {
+class ImageWidget extends StatelessWidget with ThemeMixin {
   const ImageWidget({
     required this.provider,
     this.width,
     this.height,
+    this.borderRadius,
     super.key,
   });
 
+  final ImageProvider? provider;
   final double? width;
   final double? height;
-  final ImageProvider provider;
+  final BorderRadius? borderRadius;
 
   @override
   Widget build(BuildContext context) {
-    return Image(
+    final (colors, metrics) = getTheme(context);
+
+    final radius = borderRadius ?? BorderRadius.zero;
+    final border = metrics.border.copyWith(color: colors.border);
+
+    if (provider != null) {
+      return ClipRRect(
+        borderRadius: radius,
+        child: Image(
+          width: width,
+          height: height,
+          image: provider!,
+          fit: BoxFit.cover,
+          gaplessPlayback: true,
+        ),
+      );
+    }
+
+    return ContainerWidget(
       width: width,
       height: height,
-      image: provider,
-      fit: BoxFit.cover,
-      gaplessPlayback: true,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: colors.surface,
+        borderRadius: radius,
+        border: Border.fromBorderSide(border),
+      ),
+      child: IconWidget(
+        color: colors.onSurface,
+        icon: SolarLinearIcons.gallery,
+      ),
     );
   }
 }
