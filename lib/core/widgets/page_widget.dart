5486c24bdc0d311b9acc57337a74389c90b3e7c1
João Sereia <joao.lo.sereia@gmail.com>
2024-12-03 18:49:51 -0300

    refactor(widgets/page): remove the annotated region for the status bar brightness control

--- lib/core/widgets/page_widget.dart
+++ lib/core/widgets/page_widget.dart
@@ -1,6 +1,5 @@
 import 'package:delimais_customer/core/mixins/theme_mixin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class PageWidget extends StatelessWidget with ThemeMixin {
   const PageWidget({
@@ -18,19 +17,7 @@ class PageWidget extends StatelessWidget with ThemeMixin {
 
   @override
   Widget build(BuildContext context) {
-    final theme = getThemeData(context);
     final (_, metrics) = getTheme(context);
-
-    var barBrightness = Brightness.light;
-    var barIconBrightness = Brightness.dark;
-    switch (theme.brightness) {
-      case Brightness.light:
-        barBrightness = theme.brightness;
-        barIconBrightness = Brightness.dark;
-      case Brightness.dark:
-        barBrightness = Brightness.dark;
-        barIconBrightness = theme.brightness;
-    }
 
     return Scaffold(
       extendBody: true,
@@ -43,17 +30,7 @@ class PageWidget extends StatelessWidget with ThemeMixin {
               child: appBar!,
             )
           : null,
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarBrightness: barBrightness,
-          statusBarIconBrightness: barIconBrightness,
-          systemNavigationBarColor: Colors.transparent,
-          systemNavigationBarIconBrightness: barIconBrightness,
-          systemNavigationBarDividerColor: Colors.transparent,
-        ),
-        child: Center(child: body),
-      ),
+      body: Center(child: body),
     );
   }
 }
