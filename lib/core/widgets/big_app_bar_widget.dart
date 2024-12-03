49ae31a0848d928d51ce61e819dbcadb887e7b59
João Sereia <joao.lo.sereia@gmail.com>
2024-12-03 18:49:18 -0300

    refactor(widgets/big_app_bar): remove the annotated region for the status bar brightness control

--- lib/core/widgets/big_app_bar_widget.dart
+++ lib/core/widgets/big_app_bar_widget.dart
@@ -4,7 +4,6 @@ import 'package:delimais_customer/core/widgets/image_widget.dart';
 import 'package:delimais_customer/core/widgets/safe_area_widget.dart';
 import 'package:delimais_customer/core/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:solar_icon_pack/solar_icon_pack.dart';
 
@@ -24,45 +23,35 @@ class BigAppBarWidget extends StatelessWidget with ThemeMixin {
   Widget build(BuildContext context) {
     final (_, metrics) = getTheme(context);
 
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.light,
-        systemNavigationBarColor: Colors.transparent,
-        systemNavigationBarIconBrightness: Brightness.dark,
-        systemNavigationBarDividerColor: Colors.transparent,
-      ),
-      child: Stack(
-        children: [
-          Positioned.fill(child: ImageWidget(provider: image)),
-          Positioned.fill(
-            child: ColoredBox(color: Colors.black.withOpacity(0.2)),
+    return Stack(
+      children: [
+        Positioned.fill(child: ImageWidget(provider: image)),
+        Positioned.fill(
+          child: ColoredBox(color: Colors.black.withOpacity(0.2)),
+        ),
+        Positioned.fill(
+          child: Center(
+            child: TextWidget(
+              title,
+              color: Colors.white,
+              style: TextWidgetStyle.headlineLarge,
+            ),
           ),
-          Positioned.fill(
-            child: Center(
-              child: TextWidget(
-                title,
-                color: Colors.white,
-                style: TextWidgetStyle.headlineLarge,
+        ),
+        if (isBackVisible)
+          Positioned(
+            top: 0,
+            left: metrics.medium,
+            child: SafeAreaWidget(
+              top: true,
+              child: IconButtonWidget(
+                fgColor: Colors.white,
+                icon: SolarLinearIcons.altArrowLeft,
+                onPressed: () => Get.back<void>(),
               ),
             ),
           ),
-          if (isBackVisible)
-            Positioned(
-              top: 0,
-              left: metrics.medium,
-              child: SafeAreaWidget(
-                top: true,
-                child: IconButtonWidget(
-                  fgColor: Colors.white,
-                  icon: SolarLinearIcons.altArrowLeft,
-                  onPressed: () => Get.back<void>(),
-                ),
-              ),
-            ),
-        ],
-      ),
+      ],
     );
   }
 }
